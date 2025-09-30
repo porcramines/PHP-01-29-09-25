ec3ed5e83924997886bda7a4d325f032340a926a
porcramines <porfiriones@gmail.com>
2025-09-30 11:59:37 -0500

    Add files via upload

---
+++ S2/index2.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	<center>
+<table border="1px">
+	<tr>
+		<td> <img src="imagen1.jpg"></td>
+	</tr>
+	<tr> 
+		<td>
+			<a href=" ">inicio</a>
+			<a href="">nosotros</a>
+			<a href="">ayuda</a>
+		</td>
+	</tr>
+
+</table>
+</center>
+
+<center>
+<table border="1px">
+	<tr>
+		<td>
+			<form>
+				nombre:<input type="text" name=""><br>
+				FechaNacimiento:<input type="date" name=""><br>
+				Telecono<input type="text" name=""><br>
+			</form>
+		</td>
+		<td>
+			<?php
+			$a=5;
+				print "5*5=".$a*$a;
+				print "<br>";
+				print "5+5=".$a+$a;
+				print "<br>";
+				print "5-5=".$a-$a;
+				print "<br>";
+				print "5/5=".$a/$a;
+				print "<br>";
+			?>
+		</td>
+	</tr>
+</table>
+</center>
+
+
+<table border="1px">
+	<tr>
+		<td>
+			<p>esto es un pie del documento</p>
+		</td>
+	</tr>
+</table>
+
+
+
+
+
+</body>
+</html>
